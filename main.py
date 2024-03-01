308586eb261f64801cf1b22c433e6fda8ac2c831
usiqwerty <benlogin1@protonmail.com>
2024-03-01 13:40:05 +0500

    Prompt engineering

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cached_requests import save_to_disk
 from ulearn.config import UlearnConfig
 from ulearn.courses.manager import get_course
 from ulearn.parser import parse_page
-
+from gpt.api import request
 # TODO: здесь же устанавливать рабочий каталог для homework
 # и прокинуть туда config
 
@@ -16,9 +16,9 @@ print("Project root:", file_manager.explorer.project_root)
 course = get_course(course_id)
 config = UlearnConfig(course, user_id)
 r = parse_page(pid, config)
-print(r.generate_prompt())
-# resp = request(r.generate_prompt())
-#
-# print(resp)
+print(r)
+resp = request(r.generate_prompt())
+
+print(resp)
 
 save_to_disk()
