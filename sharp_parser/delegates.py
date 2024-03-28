7b273b304f451384bcc0297a1cef69ebc88108dd
usiqwerty <benlogin1@protonmail.com>
2024-03-28 13:07:58 +0500

    Делегаты

---
+++ sharp_parser/delegates.py
@@ -0,0 +1,40 @@
+import tree_sitter
+
+import sharp_parser.sharp_types
+from sharp_parser.methods import parse_parameters
+from sharp_parser.variables import CSharpVar
+
+
+class CSharpDelegate:
+    def __init__(self, modifiers, return_type: sharp_parser.sharp_types.CSharpType, delegate_name: str, arguments: list[CSharpVar]):
+        self.modifiers = modifiers
+        self.return_type = return_type
+        self.name = delegate_name
+        self.arguments = arguments
+    def __repr__(self):
+        output = ""
+        for modifier in self.modifiers:
+            output += modifier+" "
+        output += f"delegate {self.return_type} {self.name}("
+        output += ', '.join(arg.as_param for arg in self.arguments)
+        output +=');'
+
+        return output
+
+def parse_delegate(delegate_node: tree_sitter.Node, type_resolver: sharp_parser.sharp_types.TypeResolver):
+    modifiers = []
+    return_type = None
+    delegate_name = None
+    arguments: list[CSharpVar] = []
+    for child in delegate_node.named_children:
+        match child.type:
+            case "modifier":
+                modifiers.append(child.text.decode())
+            case "identifier":
+                if not return_type:
+                    return_type = type_resolver.get_type_by_name(child.text.decode())
+                else:
+                    delegate_name = child.text.decode()
+            case "parameter_list":
+                parse_parameters(arguments, child, type_resolver)
+    return CSharpDelegate(modifiers, return_type, delegate_name, arguments)
