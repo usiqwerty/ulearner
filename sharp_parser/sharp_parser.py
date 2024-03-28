acc7576eef85b1cb5e928e3956301f695603688c
usiqwerty <benlogin1@protonmail.com>
2024-03-28 13:10:05 +0500

    Делегаты и enum-ы

--- sharp_parser/sharp_parser.py
+++ sharp_parser/sharp_parser.py
@@ -1,6 +1,8 @@
 from tree_sitter_languages import get_language, get_parser
 
 from sharp_parser.classes import parse_record, parse_class, CSharpClass
+from sharp_parser.delegates import parse_delegate
+from sharp_parser.enums import parse_enum, CSharpEnum
 from sharp_parser.interfaces import parse_interface
 from sharp_parser.namespace import CSharpNamespace
 
@@ -8,7 +10,7 @@ language = get_language('c_sharp')
 parser = get_parser('c_sharp')
 
 
-def parse_code_from_string(code: str, namespace: CSharpNamespace) -> list[CSharpClass]:
+def parse_code_from_string(code: str, namespace: CSharpNamespace) -> list[CSharpClass | CSharpEnum]:
     """
     Парсит файл и записывает в namespace
     :param code: Код на C#
@@ -36,21 +38,32 @@ def parse_code_from_string(code: str, namespace: CSharpNamespace) -> list[CSharp
         match child.type:
             case 'class_declaration':
                 ans = parse_class(child, namespace.type_resolver)
+                namespace.defined_classes.append(ans)
+                namespace.type_resolver.mark_type_as_resolved(ans.name)
                 if ans.name in namespace.type_resolver.unresolved:
                     dependencies.append(ans)
-                    namespace.defined_classes.append(ans)
-                    namespace.type_resolver.mark_type_as_resolved(ans.name)
             case "record_declaration":
                 ans = parse_record(child, namespace.type_resolver)
+                namespace.defined_classes.append(ans)
                 if ans.name in namespace.type_resolver.unresolved:
                     dependencies.append(ans)
-                    namespace.defined_classes.append(ans)
                     namespace.type_resolver.mark_type_as_resolved(ans.name)
             case "interface_declaration":
                 ans = parse_interface(child, namespace.type_resolver)
+                namespace.defined_interfaces.append(ans)
+
                 if ans.name in namespace.type_resolver.unresolved:
                     dependencies.append(ans)
-                    namespace.defined_interfaces.append(ans)
+                    namespace.type_resolver.mark_type_as_resolved(ans.name)
+            case "delegate_declaration":
+                ans = parse_delegate(child, namespace.type_resolver)
+                if ans.name in namespace.type_resolver.unresolved:
+                    dependencies.append(ans)
+                    namespace.type_resolver.mark_type_as_resolved(ans.name)
+            case "enum_declaration":
+                ans = parse_enum(child, namespace.type_resolver)
+                if ans.name in namespace.type_resolver.unresolved:
+                    dependencies.append(ans)
                     namespace.type_resolver.mark_type_as_resolved(ans.name)
 
     return dependencies
